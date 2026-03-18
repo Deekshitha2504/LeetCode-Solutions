3560e575f51a59997db932cb826f79284cfcd974
Deekshitha <deekshithalakshmiprasad@gmail.com>
2026-03-18 07:01:31 +0530

    Time: 11 ms (14.39%), Space: 12.9 MB (67.32%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,18 +5,31 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        hashmap={}
-        for j in range(len(numbers)):
-            num=numbers[j]
-            if num not in hashmap:
-                hashmap[num]=j
-            else:
-                hashmap[num]+=1
+        # hashmap={}
+        # for j in range(len(numbers)):
+        #     num=numbers[j]
+        #     if num not in hashmap:
+        #         hashmap[num]=j
+        #     else:
+        #         hashmap[num]+=1
 
-        for j in range(len(numbers)):
-            num=numbers[j] 
-            cur=target-num
-            if cur in hashmap:
-                l=[j+1,hashmap[cur]+1]
-                return l
-        return [0]
+        # for j in range(len(numbers)):
+        #     num=numbers[j] 
+        #     cur=target-num
+        #     if cur in hashmap:
+        #         l=[j+1,hashmap[cur]+1]
+        #         return l
+        # return [0]
+        left=0
+        right=len(numbers)-1
+        while left<right:
+            num1=numbers[left]
+            num2=numbers[right]
+            if num1+num2==target:
+                return [left+1, right+1]
+            elif num1+num2>target:
+                right-=1
+            else:
+                left+=1
+        return [0]                
+
