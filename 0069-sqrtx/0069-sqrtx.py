9e2cede5b5e6f15ce46f46736db9155b76c5caeb
Deekshitha <deekshithalakshmiprasad@gmail.com>
2026-03-21 08:22:55 +0530

    Time: 11 ms (18.73%), Space: 12.3 MB (76.42%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -6,12 +6,14 @@ class Solution(object):
         """
         low=1
         high=x
+        ans=0
         while low<=high:
             mid=low+(high-low)//2
             if mid**2==x:
                 return mid
             elif mid**2<x:
+                ans=mid
                 low=mid+1
             elif mid**2>x:
                 high=mid-1
-        return low-1                
+        return ans                
