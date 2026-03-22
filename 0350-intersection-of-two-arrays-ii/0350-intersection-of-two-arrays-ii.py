b8d96b9794e9d933cf97f565451411444eade498
Deekshitha <deekshithalakshmiprasad@gmail.com>
2026-03-22 12:16:13 +0530

    Time: 6 ms (29.97%), Space: 12.4 MB (83.39%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -5,9 +5,23 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        # res=[]
+        # for n1 in nums1:
+        #     if n1 in nums2:
+        #         res.append(n1)
+        #         nums2.remove(n1)
+        # return res 
+        nums1.sort()
+        nums2.sort()
+        i,j=0,0
         res=[]
-        for n1 in nums1:
-            if n1 in nums2:
-                res.append(n1)
-                nums2.remove(n1)
-        return res        
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]==nums2[j]:
+                res.append(nums1[i])
+                i+=1
+                j+=1
+            elif nums1[i]<nums2[j]:
+                i+=1
+            elif nums1[i]>nums2[j]:
+                j+=1         
+        return res                
