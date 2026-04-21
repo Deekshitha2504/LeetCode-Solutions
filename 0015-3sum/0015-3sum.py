8e26425a348fb89577f9e4171374a456ff4c8dff
Deekshitha <deekshithalakshmiprasad@gmail.com>
2026-04-21 07:20:08 +0530

    Time: 760 ms (55.06%), Space: 18.2 MB (79.8%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,21 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        sol=[]
-        nums.sort()
-        for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1]:
+        sol=[]# the final list
+        nums.sort()#sorting to find the triplets easily
+        for i in range(len(nums)):# taking i as ref
+            if i>0 and nums[i]==nums[i-1]:#to skip duplicates
                 continue
-            j,k=i+1,len(nums)-1
-            while j<k:
-                summ=nums[i]+nums[j]+nums[k]
-                if summ==0:
+            j,k=i+1,len(nums)-1 #adding two other pointers for b&c of the triplet
+            while j<k:#so that we dont keep going through the same numbers
+                summ=nums[i]+nums[j]+nums[k]# calculating the sum
+                if summ==0:# append the combi if the sum==0
                     sol.append([nums[i],nums[j],nums[k]])
-                    j+=1
-                    while nums[j]==nums[j-1] and j<k:
+                    j+=1#incrementing j to find different combi
+                    while nums[j]==nums[j-1] and j<k:# to skip duplicates
                         j+=1
-                elif summ<0:
+                elif summ<0:#if sum is lower than 0 then increment j for larger sum
                     j+=1    
-                else:
+                else:#if sum is larger than 0 then decrement k for lower sum
                     k-=1    
         return sol            
