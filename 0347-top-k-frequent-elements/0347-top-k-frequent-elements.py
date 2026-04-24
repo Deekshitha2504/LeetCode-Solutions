18108efc8dd7e001646f03faafda87fc4654a843
Deekshitha <deekshithalakshmiprasad@gmail.com>
2026-04-24 06:30:30 +0530

    Time: 0 ms (100%), Space: 22.9 MB (44.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,11 +1,9 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        counts=Counter(nums)
-        counts1=sorted(counts.items(), key=lambda items:items[1], reverse=True)
-        keyy=[x[0] for x in counts1]
-        return keyy[0:k]    
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        mapp=Counter(nums)
+        s=list(mapp.items())
+        s.sort(key= lambda x:x[1], reverse=True)
+        res=[]            
+        for x in range(k):
+            res.append(s[x][0])
+        return res    
