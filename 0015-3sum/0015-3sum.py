8e7997d55b975c20f047057204136f31849b1cfd
Deekshitha <deekshithalakshmiprasad@gmail.com>
2026-05-15 07:20:01 +0530

    Time: 651 ms (44.73%), Space: 22.1 MB (81.03%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,20 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        sol=[]# the final list
-        nums.sort()#sorting to find the triplets easily
-        for i in range(len(nums)):# taking i as ref
-            if i>0 and nums[i]==nums[i-1]:#to skip duplicates
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        res=[]
+        nums.sort()
+        for i in range(len(nums)):
+            l,r=i+1,len(nums)-1
+            if i>0 and nums[i-1]==nums[i]:
                 continue
-            j,k=i+1,len(nums)-1 #adding two other pointers for b&c of the triplet
-            while j<k:#so that we dont keep going through the same numbers
-                summ=nums[i]+nums[j]+nums[k]# calculating the sum
-                if summ==0:# append the combi if the sum==0
-                    sol.append([nums[i],nums[j],nums[k]])
-                    j+=1#incrementing j to find different combi
-                    while nums[j]==nums[j-1] and j<k:# to skip duplicates
-                        j+=1
-                elif summ<0:#if sum is lower than 0 then increment j for larger sum
-                    j+=1    
-                else:#if sum is larger than 0 then decrement k for lower sum
-                    k-=1    
-        return sol            
+            while l<r:
+                summ=nums[l]+nums[r]+nums[i]
+                if summ==0:
+                    res.append([nums[l],nums[r],nums[i]])
+                    l+=1
+                    while nums[l-1]==nums[l] and l<r:
+                        l+=1
+                elif summ<0:
+                    l+=1
+                else:
+                    r-=1
+        return res                        
