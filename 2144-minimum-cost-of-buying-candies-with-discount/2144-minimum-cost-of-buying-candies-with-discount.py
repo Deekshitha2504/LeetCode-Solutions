a703fd09fa7637ef05a37a22c6d043f4ba8b785d
Deekshitha <deekshithalakshmiprasad@gmail.com>
2026-06-02 06:57:55 +0530

    Time: 3 ms (28.8%), Space: 12.5 MB (20%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minimumCost(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        if len(cost)<=2:
+            return sum(cost)
+        total=0
+        cost.sort(reverse=True)
+        for i in range(0,len(cost)):
+            if (i+1)%3==0:        
+                continue
+            else:
+                total+=cost[i]
+        return total        
