79c9d0d95839878ded79e53135531f40a7ec4f8c
Deekshitha <deekshithalakshmiprasad@gmail.com>
2026-06-12 06:35:22 +0530

    Time: 19 ms (23.86%), Space: 20.5 MB (52.99%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,22 +1,15 @@
-class Solution(object):
-    def reverseVowels(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        vowels=['a','e','i','o','u','A','E','I','O','U']
-        sl=list(s)
-        l,r=0,len(sl)-1
-        while l<=r:
-            if sl[l] in vowels and sl[r] in vowels:
-                sl[l],sl[r]=sl[r],sl[l]
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        l,r=0,len(s)-1
+        ref=['A','a','E','e','I','i','O','o','U','u']
+        s1=list(s)
+        while l<r:
+            if s1[l] in ref and s1[r] in ref:
+                s1[l],s1[r]=s1[r],s1[l]
                 l+=1
                 r-=1
-            elif sl[l] in vowels and sl[r] not in vowels:
-                r-=1
-            elif sl[l] not in vowels and sl[r] in vowels:
+            if s1[l] not in ref:
                 l+=1
-            else:
-                l+=1
+            if s1[r] not in ref:
                 r-=1    
-        return "".join(sl)            
+        return ''.join(s1)        
